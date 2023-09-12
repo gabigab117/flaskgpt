e99ed81fb14c28a0aa110c052fcaa6f7a3ae182a
gabigab117 <gabrieltrouve5@outlook.com>
2023-09-12 22:46:55 +0200

    add css inpu api key and html for tchat

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import os
+import openai
+from dotenv import load_dotenv
 from flask import Flask, render_template
+
+
+# Va chercher le .env
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 # Création d'une instance de Flask avec le nom de notre module (app.py __name__)
@@ -11,5 +20,6 @@ def home():
 
 
 # Exécuter le code à l'intérieur de la structure conditionnelle uniquement quand on lance le fichier.
+# Si j'appelle app.py depuis un autre fichier le code ci-dessous ne s'exécute pas.
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
